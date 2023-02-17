64a88bd2dfe523cafa99e8844811c18e8566bc57
Paulo Cândido <paulo.candido@g.globo>
2023-02-16 22:28:09 -0300

    feat: add info to client logs

--- client/index.js
+++ client/index.js
@@ -5,13 +5,10 @@ const tests = [
     name: 'No cache',
     address: 'nginx-no-cache:8000',
     requests: [
-      { time: 0, simulateStatus: 200 },
-      { time: 2000, simulateStatus: 200 },
-      { time: 6000, simulateStatus: 200 },
-      { time: 30000, simulateStatus: 200 },
-      { time: 32000, simulateStatus: 500 },
-      { time: 54000, simulateStatus: 500 },
-      { time: 56000, simulateStatus: 200, simulatedResponseTime: 5000 },
+      { time: 0, simulateStatus: 200, info: 'The first request will always hit the upstream.' },
+      { time: 2000, simulateStatus: 200, info: 'As there is no cache, the second request will hit as well.' },
+      { time: 4000, simulateStatus: 200, simulatedResponseTime: 5000, info: 'Any case of time out will be exposed to the client. Note Nginx will not wait for api response.' },
+      { time: 10000, simulateStatus: 500, info: 'Any case of error will also be exposed to the client.' },
     ]
   }
 ]
@@ -24,7 +21,7 @@ async function run() {
     await Promise.all(testCase.requests.map((request, requestIndex) => new Promise(done => {
       setTimeout(async () => {
         const lastTime = testCase.requests[requestIndex - 1]?.time ?? 0
-        console.log(`Requested at t=${request.time} (${request.time - lastTime}ms from the last).`)
+        console.log(`Requested at t=${request.time} (${request.time - lastTime}ms from the last). ${request.info}`)
         try {
           await axios(`http://${testCase.address}`, {
             params: {
