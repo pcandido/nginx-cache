947e068aefc7cf8a8d38d193785e009491685268
Paulo Cândido <paulo.candido@g.globo>
2023-02-17 15:26:33 -0300

    feat: implements simple cache strategy

--- client/index.js
+++ client/index.js
@@ -10,7 +10,19 @@ const tests = [
       { time: 4000, simulateStatus: 200, simulatedResponseTime: 5000, info: 'Any case of time out will be exposed to the client. Note Nginx will not wait for api response.' },
       { time: 10000, simulateStatus: 500, info: 'Any case of error will also be exposed to the client.' },
     ]
-  }
+  },
+  {
+    name: 'Simple cache',
+    address: 'nginx-simple-cache:8000',
+    requests: [
+      { time: 0, simulateStatus: 200, info: 'The first request will always hit the upstream.' },
+      { time: 2000, simulateStatus: 200, info: 'The second request will be responded to by using the cached data (CACHE HIT). Note how faster it is.' },
+      ...(new Array(10).fill(
+        { time: 4000, simulateStatus: 200, info: 'Does not matter how many requests are done, if the cache data is there, it will be used.' },
+      )),
+      { time: 15000, simulateStatus: 200, info: 'Finally, after cached data expires, a new request will hit the upstream.' },
+    ]
+  },
 ]
 
 async function run() {
